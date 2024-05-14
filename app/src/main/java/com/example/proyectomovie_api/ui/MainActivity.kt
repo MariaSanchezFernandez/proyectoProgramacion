b04b7d68441857b309d40c357b075441141bf7f9
María Sánchez Fernández <maria.sanchez@escuelaestech.es>
2024-05-14 09:11:50 +0200

    Navegacion

--- app/src/main/java/com/example/proyectomovie_api/ui/MainActivity.kt
+++ app/src/main/java/com/example/proyectomovie_api/ui/MainActivity.kt
@@ -1,21 +1,52 @@
 package com.example.proyectomovie_api.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.proyectomovie_api.R
+import com.example.proyectomovie_api.databinding.MenuMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MenuMainBinding
+
+    //Configura las barras
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    //controlador de navegación
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = MenuMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar2)
+
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView2) as NavHost
+        navController = navHost.navController
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.fragmentInicio,
+                R.id.fragmentPeliculas,
+                R.id.fragmentSeries,
+                R.id.fragmentCuenta
+            ),binding.DrawerLayourFrame
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.drawerLayout.setupWithNavController(navController)
+
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
     }
 }
