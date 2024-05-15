2bab8259dbdbd7f86f2e65fc842e871918ce15f5
Jose Manuel Cozar Lumbreras <josemanuel.cozar@escuelaestech.es>
2024-05-15 12:33:07 +0200

    Rama nueva + diseño detalles + Fragment

--- app/src/main/java/com/example/proyectomovie_api/ui/fragmentos/Informacion.kt
+++ app/src/main/java/com/example/proyectomovie_api/ui/fragmentos/Informacion.kt
@@ -5,56 +5,48 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import com.example.proyectomovie_api.R
+import com.example.proyectomovie_api.data.movie.Movie
+import com.example.proyectomovie_api.databinding.FragmentInformacionBinding
+import com.example.proyectomovie_api.ui.MainActivity
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Informacion.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Informacion : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentInformacionBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_informacion, container, false)
+    ): View {
+        binding = FragmentInformacionBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Informacion.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Informacion().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+
+    private fun rellenaDatos(peli: Movie) {
+        val posterURL = "https://media.themoviedb.org/t/p/w300_and_h450_bestv2/" + peli.poster_path
+        val backgroundURL = "https://media.themoviedb.org/t/p/w1920_and_h800_multi_faces/" + peli.backdrop_path
+        with(binding) {
+
+            tvTituloDetalles.text = peli.title
+            tvReleaseDateDetalles.text = peli.release_date
+
+            Glide.with(requireContext())
+                .load(posterURL)
+                .into(binding.ivCartelDetalles)
+
+            Glide.with(requireContext())
+                .load(backgroundURL)
+                .into(binding.ivFondoDetalles)
+
+            (requireActivity() as MainActivity).supportActionBar?.setTitle(peli.title)
+
+        }
     }
 }
