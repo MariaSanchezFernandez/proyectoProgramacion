69c8d5efab76b05f0800b3e9cf360a8c9ef948ed
MariaSanchezFernandez <148442941+MariaSanchezFernandez@users.noreply.github.com>
2024-05-28 18:26:37 +0200

    Update Peliculas.kt
    Cambios id carousel

--- app/src/main/java/com/example/proyectomovie_api/ui/fragmentos/Peliculas.kt
+++ app/src/main/java/com/example/proyectomovie_api/ui/fragmentos/Peliculas.kt
@@ -60,7 +60,7 @@ class Peliculas : Fragment() {
 //        binding.RecyclerViewCarouselPeliculas.adapter = imageAdapter
 //        imageAdapter.submitList(imageList)
         val allImageUrls = listOf(
-            "https://image.tmdb.org/t/p/original/7Vb317OAaxJqvOXf8B5BaCRJ4kH.jpg",
+            "https://image.tmdb.org/t/p/original/7Vb317OAaxJqvOXf8B5BaCRJ4kH.jpg"),
             "https://image.tmdb.org/t/p/original/sFmTQjoFGXGOny9R1KMJqGSZ28r.jpg",
             "https://image.tmdb.org/t/p/original/xCLEq302jcV4MAKqihiBF91WIYU.jpg",
             "https://image.tmdb.org/t/p/original/AsJsO9Zac7u2WUEG0JCLyusfgUP.jpg",
@@ -89,6 +89,35 @@ class Peliculas : Fragment() {
             "https://image.tmdb.org/t/p/original/vNaCGEBjGlEMPPhmJCLVeOdUtXc.jpg",
             "https://image.tmdb.org/t/p/original/ii8IKYeVLDxz3449sbTglCLtKjb.jpg"
         )
+
+         // ImagenCarousel("118","https://image.tmdb.org/t/p/original/7Vb317OAaxJqvOXf8B5BaCRJ4kH.jpg"),
+         //    ImagenCarousel("578","https://image.tmdb.org/t/p/original/sFmTQjoFGXGOny9R1KMJqGSZ28r.jpg"),
+         //    ImagenCarousel("11","https://image.tmdb.org/t/p/original/xCLEq302jcV4MAKqihiBF91WIYU.jpg"),
+         //    ImagenCarousel("843527","https://image.tmdb.org/t/p/original/AsJsO9Zac7u2WUEG0JCLyusfgUP.jpg"),
+         //    ImagenCarousel("550","https://image.tmdb.org/t/p/original/n0ceI4oS4wCad1GPvnf4FMBwBie.jpg"),
+         //    ImagenCarousel("603","https://image.tmdb.org/t/p/original/nZi1IAiLS4UyW3PVWwN7XZWVX3M.jpg"),
+         //    ImagenCarousel("37165","https://image.tmdb.org/t/p/original/aCHn2TXYJfzPXQKA6r9mKPbMlUB.jpg"),
+         //    ImagenCarousel("640","https://image.tmdb.org/t/p/original/eNGfh7YEu2fcyWMITS12qnuMxrw.jpg"),
+         //    ImagenCarousel("10625","https://image.tmdb.org/t/p/original/pgDSpf7I9373QRqF2p7FeBshmIo.jpg"),
+         //    ImagenCarousel("423","https://image.tmdb.org/t/p/original/dVr11o9or7AS8AMPfwjSpEU83iU.jpg"),
+         //    ImagenCarousel("44214","https://image.tmdb.org/t/p/original/nsjh6qceR9ev0yI9ZSJHdWd04G7.jpg"),
+         //    ImagenCarousel("594","https://image.tmdb.org/t/p/original/zywtNiaZ9r7azrcNdl2j0jOgrkw.jpg"),
+         //    ImagenCarousel("","https://image.tmdb.org/t/p/original/topD2LQykPrT9vyQV704kUQXDlo.jpg"),
+         //    ImagenCarousel("508965","https://image.tmdb.org/t/p/original/mlxKite1x1PgmIhJgAxNS9eHmH8.jpg"),
+         //    ImagenCarousel("587","https://image.tmdb.org/t/p/original/bLqUd0tBvKezDr9MEla7k34i3rp.jpg"),
+         //    ImagenCarousel("12429","https://image.tmdb.org/t/p/original/aHU97KMJ7fSq2pyWcVraBWg9l96.jpg"),
+         //    ImagenCarousel("28178","https://image.tmdb.org/t/p/original/a5pOEjOLvr04Hr8qktIDM75OZi0.jpg"),
+         //    ImagenCarousel("4951","https://image.tmdb.org/t/p/original/4ssWRanWTKN9CQ0tfq5S1whP7tr.jpg"),
+         //    ImagenCarousel("38","https://image.tmdb.org/t/p/original/j6rZkE9ksbMsRhBvk9Az5uBc8LT.jpg"),
+         //    ImagenCarousel("11036","https://image.tmdb.org/t/p/original/zdXnJqBaGFVtLoPNuMeKfEYUViZ.jpg"),
+         //    ImagenCarousel("22","https://image.tmdb.org/t/p/original/3rlclw8qHWEX2hYHNi7dAaW1e0.jpg"),
+         //    ImagenCarousel("5528","https://image.tmdb.org/t/p/original/aHufvTTgyf6h1MDgn2t6d7jEseX.jpg"),
+         //    ImagenCarousel("155","https://image.tmdb.org/t/p/original/kxCRNTZ96dbftDPt7SQWA3LQCiK.jpg"),
+         //    ImagenCarousel("475557","https://image.tmdb.org/t/p/original/r0kZNywAeN6Ar75rxDqLlTP5RiJ.jpg"),
+         //    ImagenCarousel("856","https://image.tmdb.org/t/p/original/mED6iGO5jCD3enjE2IuINrxxZJa.jpg"),
+         //    ImagenCarousel("557","https://image.tmdb.org/t/p/original/3E9BIEHkOmkMEBddp6JMPNxwfR6.jpg"),
+         //    ImagenCarousel("551332","https://image.tmdb.org/t/p/original/vNaCGEBjGlEMPPhmJCLVeOdUtXc.jpg"),
+         //    ImagenCarousel("424694","https://image.tmdb.org/t/p/original/ii8IKYeVLDxz3449sbTglCLtKjb.jpg")
 
         // Seleccionar un subconjunto aleatorio de 6 URLs de imágenes
         val randomImageUrls = allImageUrls.shuffled().take(6)
