73be52c691105146cd162f430ae6e685d6130b7d
María Sánchez Fernández <maria.sanchez@escuelaestech.es>
2024-05-29 09:07:26 +0200

    Cambios de mis favoritos en cada fragment

--- app/src/main/java/com/example/proyectomovie_api/ui/adaptadores/AdaptadorMiListaPeliculas.kt
+++ app/src/main/java/com/example/proyectomovie_api/ui/adaptadores/AdaptadorMiListaPeliculas.kt
@@ -3,10 +3,11 @@ package com.example.proyectomovie_api.ui.adaptadores
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectomovie_api.data.movie.Movie
 import com.example.proyectomovie_api.data.movie.MovieResponse
 import com.example.proyectomovie_api.databinding.MisFavoritosPeliculasBinding
 
-class AdaptadorMiListaPeliculas(val listado: ArrayList<String>) : RecyclerView.Adapter<AdaptadorMiListaPeliculas.VistaCelda>() {
+class AdaptadorMiListaPeliculas(val listado: List<Movie>) : RecyclerView.Adapter<AdaptadorMiListaPeliculas.VistaCelda>() {
     inner class VistaCelda (val binding: MisFavoritosPeliculasBinding) : RecyclerView.ViewHolder(binding.root)
 
     interface MyClick {
