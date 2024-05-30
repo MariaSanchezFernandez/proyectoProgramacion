dfd1994df15634c094c1f4d40d93367eb61c1e96
blas-estech <blas.garcia@escuelaestech.es>
2024-05-30 16:38:30 +0200

    Adaptadores de las watchList terminado con el código

--- app/src/main/java/com/example/proyectomovie_api/ui/adapters/AdapterWLMoviesFav.kt
+++ app/src/main/java/com/example/proyectomovie_api/ui/adapters/AdapterWLMoviesFav.kt
@@ -1,4 +1,45 @@
 package com.example.proyectomovie_api.ui.adapters
 
-class AdapterWLMoviesFav {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.proyectomovie_api.data.movie.Movie
+import com.example.proyectomovie_api.databinding.HolderCarteleraFavBinding
+
+class AdapterWLMoviesFav(
+    val lista: List<Movie>,
+    val listener: FavClick
+) : RecyclerView.Adapter<AdapterWLMoviesFav.FavoritoHolder>() {
+
+    private var _binding: HolderCarteleraFavBinding? = null
+    private val binding get() = _binding!!
+
+    fun interface FavClick {
+        fun onFavClick(movie: Movie)
+    }
+
+    inner class FavoritoHolder(val binding: HolderCarteleraFavBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoritoHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = HolderCarteleraFavBinding.inflate(layoutInflater, parent, false)
+        return FavoritoHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FavoritoHolder, position: Int) {
+        val watchList = lista[position]
+        Glide.with(holder.itemView.context)
+            .load(watchList.poster_path)
+            .into(holder.binding.imgPortada)
+
+        holder.binding.clHolderFav.setOnClickListener{
+            // Click en la portada para enviarte a los detalles de la película
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
 }
--- app/src/main/java/com/example/proyectomovie_api/ui/adapters/AdapterWLTVShowsFav.kt
+++ app/src/main/java/com/example/proyectomovie_api/ui/adapters/AdapterWLTVShowsFav.kt
@@ -1,4 +1,45 @@
 package com.example.proyectomovie_api.ui.adapters
 
-class AdapterWLTVShowsFav {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.proyectomovie_api.data.tv.TVShow
+import com.example.proyectomovie_api.databinding.HolderCarteleraFavBinding
+
+class AdapterWLTVShowsFav(
+    val lista: List<TVShow>,
+    val listener: AdapterWLMoviesFav.FavClick
+) : RecyclerView.Adapter<AdapterWLTVShowsFav.FavoritoHolder>(){
+
+    private var _binding: HolderCarteleraFavBinding? = null
+    private val binding get() = _binding!!
+
+    fun interface FavClick {
+        fun onFavClick(serie: TVShow)
+    }
+
+    inner class FavoritoHolder(val binding: HolderCarteleraFavBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoritoHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = HolderCarteleraFavBinding.inflate(layoutInflater, parent, false)
+        return FavoritoHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: FavoritoHolder, position: Int) {
+        val watchList = lista[position]
+        Glide.with(holder.itemView.context)
+            .load(watchList.poster_path)
+            .into(holder.binding.imgPortada)
+
+        holder.binding.clHolderFav.setOnClickListener{
+            // Click en la portada para enviarte a los detalles de la película
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return lista.size
+    }
+
 }
