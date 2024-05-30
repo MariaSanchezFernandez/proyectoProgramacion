d0e46e9d24f3a42c06b850faf723e55e1ac56139
marcosrodriguez <marcos.rodriguez@escuelaestech.es>
2024-05-30 20:31:16 +0200

    recycler de favoritos

--- app/src/main/java/com/example/proyectomovie_api/ui/fragmentos/Inicio.kt
+++ app/src/main/java/com/example/proyectomovie_api/ui/fragmentos/Inicio.kt
@@ -15,6 +15,7 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.proyectomovie_api.R
 import com.example.proyectomovie_api.data.movie.Movie
@@ -22,6 +23,7 @@ import com.example.proyectomovie_api.data.movie.MovieResponse
 import com.example.proyectomovie_api.databinding.AlertdialogBucadorBinding
 import com.example.proyectomovie_api.databinding.FragmentInicioBinding
 import com.example.proyectomovie_api.ui.MainActivity
+import com.example.proyectomovie_api.ui.adaptadores.AdaptadorMiListaPeliculas
 import com.example.proyectomovie_api.ui.view.MyViewModel
 import com.google.android.material.search.SearchView
 
@@ -78,8 +80,45 @@ class Inicio : Fragment() {
             navController = findNavController()
 
 
+        val sesionId = myViewModel.getSessionID()
+        val acountId = myViewModel.getAccountID(sesionId.toString())
+
+        acountId.value?.let {myViewModel.getFavoriteMovies(it)}?.observe(viewLifecycleOwner){
+            configRecyclerMovies(it)
+        }
 
 
+
+    }
+
+
+    //Recoge la lista de peliculas que añades a favoritos y las muestra en un recycler de forma linearLayout
+    private fun configRecyclerMovies(lista : List<Movie>){
+        myViewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
+            myViewModel.getAccountDetails(sessionId).observe(viewLifecycleOwner){listaFavoritos ->
+                val adaptadorFavoritos = AdaptadorMiListaPeliculas(lista, object : AdaptadorMiListaPeliculas.MyClick{
+                    override fun onHolderClick(pelicula: Movie) {
+                        val id = pelicula.id
+                        myViewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
+                            if (it !== null){
+                                myViewModel.setPelicula(it)
+                                findNavController().navigate(R.id.action_favoritos_to_informacion)
+                            }
+                        }
+                    }
+
+                    override fun onItemLongClick(pelicula: Movie) {
+
+                    }
+
+                })
+
+                binding.recyclerViewPeliculasTuLista.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+                binding.recyclerViewPeliculasTuLista.adapter = adaptadorFavoritos
+
+            }
+
+        }
     }
 
 
