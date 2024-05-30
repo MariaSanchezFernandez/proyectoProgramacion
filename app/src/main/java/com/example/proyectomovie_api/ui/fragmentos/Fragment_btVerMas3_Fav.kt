f5238b0cc4bdf3d3b6b99a6e1f65c98223dc3881
blas-estech <blas.garcia@escuelaestech.es>
2024-05-30 19:19:11 +0200

    Arreglos en el fragment al que navega al darle al botón de ver más que hay en el layout de favoritos

--- app/src/main/java/com/example/proyectomovie_api/ui/fragmentos/Fragment_btVerMas3_Fav.kt
+++ app/src/main/java/com/example/proyectomovie_api/ui/fragmentos/Fragment_btVerMas3_Fav.kt
@@ -12,16 +12,19 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.proyectomovie_api.R
 import com.example.proyectomovie_api.data.movie.Movie
 import com.example.proyectomovie_api.data.tv.TVShow
+import com.example.proyectomovie_api.data.watchlist.addWatchListBody
 import com.example.proyectomovie_api.databinding.FragmentBtVerMas3FavBinding
 import com.example.proyectomovie_api.ui.adaptadores.AdaptadorMiListaPeliculas
 import com.example.proyectomovie_api.ui.adaptadores.AdaptadorMiListaSerie
+import com.example.proyectomovie_api.ui.adapters.AdapterWLMoviesFav
+import com.example.proyectomovie_api.ui.adapters.AdapterWLTVShowsFav
 import com.example.proyectomovie_api.ui.view.MyViewModel
 
 class fragment_btVerMas3_Fav : Fragment() {
 
     private var _binding: FragmentBtVerMas3FavBinding? = null
     private val binding get() = _binding!!
-    private val viewModel by  activityViewModels<MyViewModel>()
+    private val ViewModel by  activityViewModels<MyViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,34 +37,32 @@ class fragment_btVerMas3_Fav : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //myViewModel.getFavMovies().observe(viewLifecycleOwner){}
-        //myViewModel.getFavTVShows().observe(viewLifecycleOwner){}
-
-        //configRecyclerMoviesInter()
-        //configRecyclerTvShowsInter()
-        //configRecyclerMoviesWlInter()
-        //configRecyclerTvShowsWlInter()
+        configRecyclerMoviesInter()
+        configRecyclerTvShowsInter()
+        configRecyclerMoviesWlInter()
+        configRecyclerTvShowsWlInter()
 
     }
 
-    /*
     private fun configRecyclerMoviesInter() {
-        viewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
-            viewModel.getAccountDetails(sessionId).observe(viewLifecycleOwner){accountId ->
-                viewModel.getFavoriteMovies(accountId.id).observe(viewLifecycleOwner){listaFavoritos ->
+        ViewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
+            ViewModel.getAccountDetails(sessionId).observe(viewLifecycleOwner){accountId ->
+                ViewModel.getFavoriteMovies(accountId.id).observe(viewLifecycleOwner){ listaFavoritos ->
                     val adaptadorFavoritos = AdaptadorMiListaPeliculas(listaFavoritos, object : AdaptadorMiListaPeliculas.MyClick{
                         override fun onHolderClick(pelicula: Movie) {
                             val id = pelicula.id
-                            viewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
+                            ViewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
                                 if (it != null) {
-                                    viewModel.setPelicula(it)
-                                    findNavController().navigate(R.id.action_favoritos_to_informacion)
+                                    ViewModel.setPelicula(it)
+                                    findNavController().navigate(R.id.action_fragment_btVerMas3_Fav_to_informacion)
                                 }
                             }
                         }
+                        override fun onItemLongClick(pelicula: Movie) {
 
+                        }
                     })
-                    binding.rvFavInter.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                    binding.rvFavInter.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                     binding.rvFavInter.adapter = adaptadorFavoritos
                 }
             }
@@ -69,21 +70,24 @@ class fragment_btVerMas3_Fav : Fragment() {
     }
 
     private fun configRecyclerTvShowsInter() {
-        viewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
-            viewModel.getAccountDetails(sessionId).observe(viewLifecycleOwner){accountId ->
-                viewModel.getFavoriteTVShows(accountId.id).observe(viewLifecycleOwner){listaTVShow ->
+        ViewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
+            ViewModel.getAccountDetails(sessionId).observe(viewLifecycleOwner){accountId ->
+                ViewModel.getFavoriteTVShows(accountId.id).observe(viewLifecycleOwner){listaTVShow ->
                     val adaptadorFavoritos = AdaptadorMiListaSerie(listaTVShow, object : AdaptadorMiListaSerie.MyClick{
                         override fun onHolderClick(serie: TVShow) {
                             val id = serie.id
-                            viewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
+                            ViewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
                                 if (it != null) {
-                                    viewModel.setPelicula(it)
-                                    findNavController().navigate(R.id.action_favoritos_to_informacion)
+                                    ViewModel.setPelicula(it)
+                                    findNavController().navigate(R.id.action_fragment_btVerMas3_Fav_to_informacion)
                                 }
                             }
                         }
+                        override fun onItemLongClick(serie: TVShow) {
+
+                        }
                     })
-                    binding.rvFavInter.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                    binding.rvFavInter.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                     binding.rvFavInter.adapter = adaptadorFavoritos
                 }
             }
@@ -91,22 +95,21 @@ class fragment_btVerMas3_Fav : Fragment() {
     }
 
     private fun configRecyclerMoviesWlInter() {
-        viewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
-            viewModel.getAccountID(sessionId).observe(viewLifecycleOwner){accountId ->
-                viewModel.getFavoriteWatchListMovies(accountId).observe(viewLifecycleOwner){listaFavoritos ->
-                    val adaptadorFavoritos = AdaptadorMiListaPeliculas(listaFavoritos, object : AdaptadorMiListaPeliculas.MyClick{
-                        override fun onHolderClick(pelicula: Movie) {
-                            val id = pelicula.id
-                            viewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
+        ViewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
+            ViewModel.getAccountID(sessionId).observe(viewLifecycleOwner){accountId ->
+                ViewModel.getFavoriteWatchListMovies(accountId).observe(viewLifecycleOwner){ listaWatchListMovies ->
+                    val adaptadorFavoritos = AdapterWLMoviesFav(listaWatchListMovies, object : AdapterWLMoviesFav.FavClick{
+                        override fun onHolderClick(addWatchListBody: addWatchListBody) {
+                            val id = addWatchListBody.media_id
+                            ViewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
                                 if (it != null) {
-                                    viewModel.setPelicula(it)
-                                    findNavController().navigate(R.id.action_favoritos_to_informacion)
+                                    ViewModel.setPelicula(it)
+                                    findNavController().navigate(R.id.action_fragment_btVerMas3_Fav_to_informacion)
                                 }
                             }
                         }
-
                     })
-                    binding.rvFavInter.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                    binding.rvFavInter.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                     binding.rvFavInter.adapter = adaptadorFavoritos
                 }
             }
@@ -114,27 +117,25 @@ class fragment_btVerMas3_Fav : Fragment() {
     }
 
     private fun configRecyclerTvShowsWlInter() {
-        viewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
-            viewModel.getAccountID(sessionId).observe(viewLifecycleOwner){accountId ->
-                viewModel.getFavouriteWatchListTVShows(accountId).observe(viewLifecycleOwner){listaTVShow ->
-                    val adaptadorFavoritos = AdaptadorMiListaSerie(listaTVShow, object : AdaptadorMiListaSerie.MyClick{
-                        override fun onHolderClick(serie: TVShow) {
-                            val id = serie.id
-                            viewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
+        ViewModel.getSessionID().observe(viewLifecycleOwner){sessionId ->
+            ViewModel.getAccountID(sessionId).observe(viewLifecycleOwner){accountId ->
+                ViewModel.getFavouriteWatchListTVShows(accountId).observe(viewLifecycleOwner){ listaWatchListTVShows ->
+                    val adaptadorFavoritos = AdapterWLTVShowsFav(listaWatchListTVShows, object : AdapterWLTVShowsFav.FavClick{
+                        override fun onHolderClick(addWatchListBody: addWatchListBody) {
+                            val id = addWatchListBody.media_id
+                            ViewModel.getMovieById(id, "es-ES").observe(viewLifecycleOwner){
                                 if (it != null) {
-                                    viewModel.setPelicula(it)
-                                    findNavController().navigate(R.id.action_favoritos_to_informacion)
+                                    ViewModel.setPelicula(it)
+                                    findNavController().navigate(R.id.action_fragment_btVerMas3_Fav_to_informacion)
                                 }
                             }
                         }
-
                     })
-                    binding.rvFavInter.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+                    binding.rvFavInter.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
                     binding.rvFavInter.adapter = adaptadorFavoritos
                 }
             }
         }
     }
-    */
 
 }
